eea737a797536ad696eacdff754c227dcdbb12e5
AngeloBosetti <150057418+AngeloBosetti@users.noreply.github.com>
2024-04-12 09:07:28 +0200

    mise en place de sectionOpen dans les sections

--- src/pages/accordeon.vue
+++ src/pages/accordeon.vue
@@ -1,18 +1,19 @@
 <script setup lang="ts">
   import { ref } from 'vue'
-  const maVar = ref(true)
-  const maVar2 = ref(1)
+  const sectionOpen = ref(1)
 </script>
 
 <template>
-    <h1>ici page de l'Accordéon</h1>
-    <button ></button>
-    <button @click="maVar2 = 1">Section 1</button>
-        <h2 v-show="maVar2 === 1" class="text-2xl">Lien de page 1</h2>
-    <button @click="maVar2 = 2">Section 2</button>
-        <h2 v-show="maVar2 === 2" class="text-2xl">Lien de page 1</h2>
-    <button @click="maVar2 = 3">3</button>
-        <h2 v-show="maVar2 === 3" class="text-2xl">ca vaut 3</h2>
-    <button @click="maVar2 =  4">4</button>
-        <h2 v-show="maVar2 === 4" class="text-2xl">ca vaut 4</h2>
+    <section>
+        <button class="text-xl" @pointerdown="sectionOpen = 1">bouton 1</button>
+        <p v-show="sectionOpen === 1">Lorem ipsum dolor sit amet consectetur adipisicing elit. Odio ratione nobis accusamus esse, quaerat qui corporis similique rerum laborum quod, modi eos reprehenderit pariatur voluptates quia nostrum iusto! Molestiae, doloribus!</p>
+    </section>
+    <section>
+        <button class="text-xl" @pointerdown="sectionOpen = 2">bouton 2</button>
+        <p v-show="sectionOpen === 2">Aliquid eum quod officiis ipsa quidem modi magni repudiandae consectetur veniam recusandae, est rem? Quibusdam ipsa voluptates illo at consequatur, accusantium assumenda odio est tempore optio reprehenderit ex. Exercitationem, consequuntur!</p>
+    </section>
+    <section>
+        <button class="text-xl" @pointerdown="sectionOpen = 3">bouton 3</button>
+        <p v-show="sectionOpen === 3">Vel velit quod enim. Voluptatibus officiis aperiam non quaerat voluptates commodi, totam optio eligendi deserunt quae? Beatae quisquam fugit itaque velit asperiores officia, architecto nemo laborum dicta est quam ab?</p>
+    </section>
 </template>
